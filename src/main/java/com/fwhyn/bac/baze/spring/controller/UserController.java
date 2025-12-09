804d7d732d50d0816b716a0def3f49511b9e7e8a
Yana Wahyuna <yana.wahyuna@epson.co.id>
2025-12-09 17:13:43 +0700

    Project Config

--- src/main/java/com/fwhyn/bac/baze/spring/controller/UserController.java
+++ src/main/java/com/fwhyn/bac/baze/spring/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.fwhyn.bac.baze.spring.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fwhyn.bac.baze.spring.model.User;
 import com.fwhyn.bac.baze.spring.service.UserService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
